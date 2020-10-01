0ea9ea7f8a0a580faea177d723cb91b697b71b47
Genaro Farias <>
2020-10-01 15:22:07 -0300

    Correciones varias. Se remplaza el for del metodo borrar por un iterador. Con el for solo borraba el primer dato que encontraba y no tenia en cuenta si habia mas datos para borrar.

--- src/lab_tpn7_git/DirectorioTelefonico.java
+++ src/lab_tpn7_git/DirectorioTelefonico.java
@@ -4,19 +4,24 @@ import java.util.*;
 
 
 public class DirectorioTelefonico {
-    Map<Long,Cliente> clientes = new TreeMap<>();
+    private TreeMap<Long,Cliente> clientes = new TreeMap<>();
 
     public DirectorioTelefonico() {
     }
     
     
     
-    public void agregarCliente(Long telefono, Cliente cliente){
+    public void agregarCliente(long telefono, Cliente cliente){
+        if(!clientes.containsKey(telefono)){
         clientes.put(telefono, cliente);
+            System.out.println("Se registro el cliente "+cliente.getApellido()+" "+cliente.getNombre()+" y el numero "+telefono);
         
+    }else{
+            System.out.println("El telefono "+telefono+" ya esta registrado y asociado a un cliente");
+        }
     }
     
-    public Cliente buscarCliente(Long telefono){
+    public Cliente buscarCliente(long telefono){
         if(clientes.containsKey(telefono)){
         return clientes.get(telefono);
         }
@@ -28,7 +33,7 @@ public class DirectorioTelefonico {
     
     public Set<Long> buscarTelefono(String apellido){
        HashSet<Long>listaDeTel = new HashSet<>();
-       HashSet<Long>listaDeDni = new HashSet<>();
+       
  
        Long claveUnica=0L;
        for (Map.Entry<Long,Cliente> cliente: clientes.entrySet()){
@@ -47,20 +52,50 @@ public class DirectorioTelefonico {
     
     
     public void borrarCliente(long dni){
-        Long c=0L;
-        String elC=null;
-        for (Map.Entry<Long,Cliente> cliente: clientes.entrySet()){
-           Long clave= cliente.getKey();
-           Cliente elCliente = cliente.getValue();
-           long dniCliente= elCliente.getDni();
-           
-           if(dniCliente==dni){
-               c=clave;
-               elC=elCliente.getApellido();      
-           }
-        }
-        System.out.println("El Cliente "+elC+" fue removido");
-        clientes.remove(c);
+               
+            Set<Long> claves=clientes.keySet();
+            Iterator<Long> it=claves.iterator();
+            Long key;
+            
+            while(it.hasNext()){
+                key=it.next();
+                long aComparar = clientes.get(key).getDni();
+                System.out.println("Se comparo "+aComparar+" "+dni);
+                
+                if(aComparar==dni){
+                    System.out.println("Hay una coincidencia. El Cliente "+clientes.get(key).getApellido()+" fue removido");
+                    it.remove();
+                    clientes.remove(key);
+                }
+            }
         
+        
+        
+        
+ 
+//        Long c=0L;        // Se comenta porque funciona pero borra un solo registro, el primero que encuetra.
+//        String elC=null;
+//        for (Map.Entry<Long,Cliente> cliente: clientes.entrySet()){
+//           Long clave= cliente.getKey();
+//           Cliente elCliente = cliente.getValue();
+//           long dniCliente= elCliente.getDni();
+//           
+//           if(dniCliente==dni){
+//               c=clave;
+//               elC=elCliente.getApellido();  
+//               }
+//        }
+//        System.out.println("El Cliente "+elC+" fue removido");
+//        clientes.remove(c);
+        
+    }
+    
+    public void imprimirClientes(){
+        int n=1;
+        for(Long it:clientes.keySet()){
+            
+            System.out.println(n+" telefono "+it+" Dni "+clientes.get(it).getDni()+" Nombre "+clientes.get(it).getApellido()+" "+clientes.get(it).getNombre());
+            n++;
+        }
     }
 }
--- src/lab_tpn7_git/Lab_TpN7_Git.java
+++ src/lab_tpn7_git/Lab_TpN7_Git.java
@@ -7,48 +7,53 @@ public class Lab_TpN7_Git {
 
     public static void main(String[] args) {
         Cliente cli1=new Cliente("Genaro", "Farias", 26525567, "San Luis ", "Italia 2332");
-        Cliente cli2=new Cliente("Abril", "Castro", 44567987, "Mendoza", "Holanda 2332");
+        Cliente cli2=new Cliente("Abril", "Farias", 44567987, "Mendoza", "Holanda 2332");
         Cliente cli3=new Cliente("Patricia", "Garro", 27954943, "San Luis ", "Panama 2332");
         Cliente cli4=new Cliente("Juan", "Perez", 26999888, "Los Angeles ", "Brasil 2332");
         Cliente cli5=new Cliente("Francisco", "Carreño", 34897654, "San Luis ", "San martin 2332");
     
         DirectorioTelefonico Dir2020 = new DirectorioTelefonico();
     
-        Dir2020.agregarCliente(2664692950L, cli1);
-        Dir2020.agregarCliente(2664666098L, cli4);
-        Dir2020.agregarCliente(2664789342L, cli3);
-        Dir2020.agregarCliente(2664732850L, cli2);
-        Dir2020.agregarCliente(2661236598L, cli1);
-        Dir2020.agregarCliente(2664555555L, cli5);
-        Dir2020.agregarCliente(2664666666L, cli2);
+        Dir2020.agregarCliente(2664100, cli1);
+        Dir2020.agregarCliente(2664124, cli4);
+        Dir2020.agregarCliente(2664135, cli3);
+        Dir2020.agregarCliente(2664109, cli2);
+        Dir2020.agregarCliente(2664102, cli1);
+        Dir2020.agregarCliente(2664199, cli5);
+        Dir2020.agregarCliente(2664102, cli2);
+        Dir2020.agregarCliente(2664178, cli5);
         
+        System.out.println("----------------------------------------------------");
+        
+        Dir2020.imprimirClientes();
+        
+        System.out.println("----------------------------------------------------");
         
           
        System.out.println("Se desea buscar un cliente por su telefono");
-       long telefono=2664692950L;
+       long telefono=2664102;
        System.out.println("El cliente con el numero "+telefono+" es "+Dir2020.buscarCliente(telefono).getApellido());
        
-        System.out.println("");
+       System.out.println("----------------------------------------------------");
+        
         
        System.out.println("Se desea buscar los telefono del cliente por su apellido");
        String apellido="farias";
-        
-       
-      System.out.println("Lista de numeros del Cliente "+apellido+":");
-      Iterator ite;
-      int c=1;
+       System.out.println("Lista de numeros del Cliente "+apellido+":");
+       Iterator ite;
+       int c=1;
         ite = Dir2020.buscarTelefono(apellido).iterator();
        while(ite.hasNext()){
             System.out.println("Numero "+c+": "+ ite.next());
             c++;
        }
-       
+       System.out.println("----------------------------------------------------");   
         System.out.println("Muestro los numeros de "+apellido+" "+Dir2020.buscarTelefono(apellido)); //Otra forma mas simple de mostrarlos sin el iterador
+       System.out.println("----------------------------------------------------");    
         
-        
-     Dir2020.borrarCliente(44567987);
-     
-    
+       Dir2020.borrarCliente(26525567);
+       System.out.println("----------------------------------------------------"); 
+       Dir2020.imprimirClientes();
     
     }
     
